63f30fe54715def162b37f8def8beb5916bf3af4
Zara <zaramckeown@codingfury.com>
2015-12-17 21:56:38 +0000

    First Commit
    Bootstrap Complete

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Soblog::Application.routes.draw do
+  root :to => "articles#index"
+  resources :articles do
+    resources :comments
+  end
+  resources :users
+  resource :session
+  get '/login' => "sessions#new", :as => "login"
+  get '/logout' => "sessions#destroy", :as => "logout"
+end
